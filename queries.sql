06b4fcaee7a0ccf43af3ad6d8fbe360522a54fd6
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-30 15:02:24 -0600

    Inserted sql commands

--- queries.sql
+++ queries.sql
@@ -1,1 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
+
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
+SELECT * from animals WHERE neutered = 't' AND escape_attempts < 3;
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered = 't';
+SELECT * from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+ 
